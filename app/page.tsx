451f4c398bd081fd35a09e1716e216e942faf79f
fabell-dev <ojeafabian66@gmail.com>
2026-02-18 16:24:31 -0500

    changing hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-        </div>
-      </div>
-    </main>
+    // <main className="min-h-screen flex flex-col items-center">
+    //   <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    //     <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+    <Hero />
+    //     </div>
+    //   </div>
+    // </main>
   );
 }
